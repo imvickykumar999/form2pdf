1356c481110ef26c0da5e4369f78f9defc094579
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-11 19:00:54 +0530

    pdf merger

--- vicks/text2pdf.py
+++ vicks/text2pdf.py
@@ -1,6 +1,6 @@
 # pip install fpdf
 
-def convert():
+def convert(pdf_path = 'myfile.txt'):
     from fpdf import FPDF
     # save FPDF() class into
     # a variable pdf
@@ -14,15 +14,24 @@ def convert():
     pdf.set_font("Arial", size = 15)
 
     # open the text file in read mode
-    f = open("myfile.txt", "r")
+    f = open(pdf_path, "r")
 
     # insert the texts in pdf
     for x in f:
         pdf.cell(200, 10, txt = x, ln = 1)
 
     # save the pdf with name .pdf
-    pdf.output("myfile.pdf")
+    pdf.output(f'{pdf_path.split(".")[0]}.pdf')
 
     # uncomment to ...password protect
     # from vicks import encrypt as enc
     # enc.encryptpdf("/myfile.pdf")
+
+# convert('comments.json')
+
+# from PyPDF2 import PdfMerger, PdfReader
+# merger = PdfMerger()
+
+# merger.append(PdfReader(open(pdfname1 + ".pdf", 'rb')))
+# merger.append(PdfReader(open(pdfname2 + ".pdf", 'rb')))
+# merger.write("merged.pdf")
